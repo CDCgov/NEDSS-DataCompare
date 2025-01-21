295fdd708a524d668c3b4e0d6d9012aeb9a8cdfc
Duc Nguyen <dnguyen@enquizit.com>
2025-01-20 19:53:32 -0700

    moving email to processor

---
+++ DataCompareProcessor/src/main/java/gov/cdc/datacompareprocessor/kafka/KafkaEmailConsumerService.java
@@ -0,0 +1,32 @@
+package gov.cdc.datacompareprocessor.kafka;
+
+import com.google.gson.Gson;
+import gov.cdc.datacompareprocessor.service.EmailService;
+import gov.cdc.datacompareprocessor.service.model.EmailEventModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaEmailConsumerService {
+    private final EmailService emailService;
+    private final Gson objectMapper;
+
+    public KafkaEmailConsumerService(EmailService emailService, Gson objectMapper) {
+        this.emailService = emailService;
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener(topics = "${kafka.topic.data-compare-email-topic}")
+    public void handleMessage(String message) {
+        try {
+            EmailEventModel emailEvent = objectMapper.fromJson(message, EmailEventModel.class);
+            log.info("Processing email request for file: {}", emailEvent.getFileName());
+            emailService.sendComparisonEmail(emailEvent);
+            log.info("Email sent successfully for file: {}", emailEvent.getFileName());
+        } catch (Exception e) {
+            log.error("Failed to process email request: {}", e.getMessage(), e);
+        }
+    }
+}
