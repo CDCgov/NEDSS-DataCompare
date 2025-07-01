803a84159aff630a0bc9dfc2b87f04c1fda7739c
agonzalez-cloud39 <50598734+agonzalez-cloud39@users.noreply.github.com>
2025-07-01 14:07:05 -0400

    Adding keys and RDB_LAST_REFRESH_TIME to ignore field list (#35)
    Co-authored-by: Ariel Gonzalez <agonzalez@ignw.io>

--- DataCompareAPIs/src/main/resources/sql/dataCompareDataGeneration.sql
+++ DataCompareAPIs/src/main/resources/sql/dataCompareDataGeneration.sql
@@ -826,7 +826,7 @@ values ('D_PATIENT', 'RDB', 'RDB_MODERN',
        		'SELECT COUNT(*)
                                       FROM LAB_RESULT_COMMENT;',
        		'LAB_TEST_UID',
-       		'RowNum, LAB_TEST_UID, LAB_RESULT_COMMENT_KEY, RESULT_COMMENT_GRP_KEY',
+       		'RowNum, LAB_TEST_UID, LAB_RESULT_COMMENT_KEY, RESULT_COMMENT_GRP_KEY, RDB_LAST_REFRESH_TIME',
        		1
        		),
        	(
@@ -844,7 +844,7 @@ values ('D_PATIENT', 'RDB', 'RDB_MODERN',
        		'SELECT COUNT(*)
                                       FROM LAB_RESULT_VAL;',
        		'LAB_TEST_UID',
-       		'RowNum, LAB_TEST_UID',
+       		'RowNum, LAB_TEST_UID, TEST_RESULT_GRP_KEY, TEST_RESULT_VAL_KEY, RDB_LAST_REFRESH_TIME',
        		1
        		),
        	(
